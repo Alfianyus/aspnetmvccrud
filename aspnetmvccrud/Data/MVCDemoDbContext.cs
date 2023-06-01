65256b0493afe3e76ea2b594e79087fed2338fa5
alfian.yuska <alfian.yuska@phiraka.com>
2023-06-02 05:27:59 +0700

    Add project files.

---
+++ aspnetmvccrud/Data/MVCDemoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using aspnetmvccrud.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace aspnetmvccrud.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
