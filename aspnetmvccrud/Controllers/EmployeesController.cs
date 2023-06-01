ba508359a3676680c44f5775f9a0428912124bd4
alfian.yuska <alfian.yuska@phiraka.com>
2023-06-02 06:17:50 +0700

    UpdateEmployeeController

--- aspnetmvccrud/Controllers/EmployeesController.cs
+++ aspnetmvccrud/Controllers/EmployeesController.cs
@@ -1,13 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using aspnetmvccrud.Data;
+using aspnetmvccrud.Models;
+using aspnetmvccrud.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
 
 namespace aspnetmvccrud.Controllers
 {
     public class EmployeesController : Controller
     {
+        private readonly MVCDemoDbContext mvcDemoDbContext;
+
+        public EmployeesController(MVCDemoDbContext mvcDemoDbContext)
+        {
+            this.mvcDemoDbContext = mvcDemoDbContext;
+        }
+
         [HttpGet]
         public IActionResult Add()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult>Add(AddEmployeeViewModel addEmployeeRequest)
+        {
+            var employee = new Employee()
+            {
+                Id = Guid.NewGuid(),
+                Name = addEmployeeRequest.Name,
+                Email = addEmployeeRequest.Email,
+                Salary = addEmployeeRequest.Salary,
+                Department = addEmployeeRequest.Department,
+                DateOfBirth = addEmployeeRequest.DateOfBirth,
+            };
+
+            await mvcDemoDbContext.Employees.AddAsync(employee);
+            await mvcDemoDbContext.SaveChangesAsync();
+            return RedirectToAction("Add");
         }
     }
 }
